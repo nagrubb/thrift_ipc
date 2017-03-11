e955a47b5e10a044bdc846b3eb7efbb48e312aee
Nathan Grubb <nate@evidence.com>
2017-03-11 10:49:34 -0800

    Everything works

--- client.cpp
+++ client.cpp
@@ -12,18 +12,20 @@ using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 
 int main(int argc, char **argv) {
-  boost::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
-  boost::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-  boost::shared_ptr<TProtocol> protocol(new TCompactProtocol(transport));
+  boost::shared_ptr<TSocket>    socket(new TSocket("localhost", 9090));
+  boost::shared_ptr<TTransport> transport(new TFramedTransport(socket));
+  boost::shared_ptr<TProtocol>  protocol(new TCompactProtocol(transport));
 
   ExampleClient client(protocol);
+
   transport->open();
   clock_t begin = clock();
+
   for (int i = 0; i < 1000; ++i) {
     client.ping();
   }
-  clock_t end = clock();
-  double elapsed_secs = double(end - begin) / (CLOCKS_PER_SEC / 1000);
+  clock_t end          = clock();
+  double  elapsed_secs = double(end - begin) / (CLOCKS_PER_SEC / 1000);
   cout << "Elapsed Time: " << elapsed_secs << "ms" << endl;
   transport->close();
 
